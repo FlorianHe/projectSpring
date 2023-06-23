be2aa60a423b331d3fb405305445343b56e286c3
FlorianHe <florian.helaine@epitech.eu>
2023-06-23 16:30:33 +0200

    Quest 2

---
+++ quest-spring-jpa-onetomany/src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -0,0 +1,67 @@
+package com.wildcodeschool.wildandwizard.entity;
+
+import com.wildcodeschool.wildandwizard.entity.Wizard;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class School {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long capacity;
+    private String country;
+    @OneToMany(mappedBy = "school")
+    private List<Wizard> wizards;
+
+    public School() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Long capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public List<Wizard> getWizards() {
+        return wizards;
+    }
+
+    public void setWizards(List<Wizard> wizards) {
+        this.wizards = wizards;
+    }
+}
