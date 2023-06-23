0172bc36d841c5746586c5c8828eae21e73eca14
FlorianHe <florian.helaine@epitech.eu>
2023-06-23 16:48:29 +0200

    Quest 3

---
+++ quest-spring-jpa-manytomany/src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -0,0 +1,51 @@
+package com.wildcodeschool.wildandwizard.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import java.util.List;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards;
+
+    public Course() {
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Wizard> getWizards() {
+        return wizards;
+    }
+
+    public void setWizards(List<Wizard> wizards) {
+        this.wizards = wizards;
+    }
+}
